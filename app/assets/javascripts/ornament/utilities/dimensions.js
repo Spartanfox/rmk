628a6fbdc6ab039eba9c171b421e5f1222dc7a86
Peter Vawser <petervawser@Peters-MacBook-Pro-2.local>
2018-01-22 16:07:38 +1030

    added ornament

---
+++ app/assets/javascripts/ornament/utilities/dimensions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Ornament.windowWidth = function(){
+  return window.document.documentElement.clientWidth;
+}
+
+Ornament.windowHeight = function(){
+  return window.document.documentElement.clientHeight;
+}
